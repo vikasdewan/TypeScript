6fa8d36731d142acec50868efcb3f1c427beb678
VikasDewangan <dewanganvikas192@gmail.com>
2025-06-04 11:53:50 +0530

    15 min done | 2 videos done | type aliases | readonly and optional(?)

--- 02Basics/myObjects.js
+++ 02Basics/myObjects.js
@@ -1,25 +1,16 @@
 "use strict";
+// const User  = {
+//     Auth : {
+//         name :  "vikas",       
+//         email : "vikas@gmail.com",
+//         password : "1234",
+//     },
 Object.defineProperty(exports, "__esModule", { value: true });
-var User = {
-    Auth: {
-        name: "vikas",
-        email: "vikas@gmail.com",
-        password: "1234",
-    },
-    Education: {
-        degree: "Btech",
-        college: "IIT",
-        year: "2018",
-    },
+var myUser = {
+    _id: "321342",
+    name: "vikas",
+    email: "vikas@gmail.com",
+    isActive: true,
+    age: 22
 };
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPlaced;
-}
-;
-var newUser = { name: "vikas", isPlaced: true, package: 1200000 };
-// createUser({name: "vikas", isPlaced : true});
-createUser(newUser);
-function createStudent() {
-    return { name: "vikas", class: 12 };
-}
-;
+myUser.name = "shubham";
--- 02Basics/myObjects.ts
+++ 02Basics/myObjects.ts
@@ -1,26 +1,93 @@
-const User  = {
-    Auth : {
-        name :  "vikas",       
-        email : "vikas@gmail.com",
-        password : "1234",
-    },
+// const User  = {
+//     Auth : {
+//         name :  "vikas",       
+//         email : "vikas@gmail.com",
+//         password : "1234",
+//     },
 
-    Education : {
-        degree : "Btech",
-        college : "IIT",
-        year : "2018",
-    },
+//     Education : {
+//         degree : "Btech",
+//         college : "IIT",
+//         year : "2018",
+//     },
+// }
+
+// function createUser({name: string, isPlaced:boolean}){};
+
+// let newUser = {name:"vikas", isPlaced:true , package:1200000};
+
+// // createUser({name: "vikas", isPlaced : true});
+// createUser(newUser);
+
+// function createStudent():{name:string,class:number}{
+//     return {name:"vikas" , class:12}
+// };
+
+
+
+
+
+
+
+//Alias
+
+// type User = {
+//         name : string;
+//         email : string;
+//         isActive : boolean;
+//         age : number;
+// }
+
+// function createUser(user : User) : string{
+//     return "user created Successfully";
+// }
+
+// createUser({name:"",email:"",isActive:true,age:22})
+
+
+
+
+
+//readonly.
+
+type User = {
+   readonly _id: string
+    name : string
+    email : string
+    isActive : boolean
+    age : number
+    credcardDetails ? : number  // ? is use for optional.
 }
 
-function createUser({name: string, isPlaced:boolean}){};
+let myUser : User = {
+    _id : "321342",
+    name:"vikas",
+    email:"vikas@gmail.com",
+    isActive:true,
+    age:22
+    
+}
 
-let newUser = {name:"vikas", isPlaced:true , package:1200000};
+myUser.name = "shubham";
+// myUser._id = "2343";   give error
 
-// createUser({name: "vikas", isPlaced : true});
-createUser(newUser);
 
-function createStudent():{name:string,class:number}{
-    return {name:"vikas" , class:12}
-};
+
+type cardNum = {
+    cardnumder : number
+}
+
+type cardExpDate = {
+    cardExpireDate : number
+    }
+
+type cardCVVNum = {
+    cardCvvNumber : number
+}
+
+type cardDetails = cardNum & cardExpDate & cardCVVNum  & {
+    accBalance : string
+}
+
 
 export{}
