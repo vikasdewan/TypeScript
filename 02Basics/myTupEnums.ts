b7a11db588390e3341a6b0d763479f5c01b46c6c
VikasDewangan <dewanganvikas192@gmail.com>
2025-06-05 22:28:31 +0530

    tuples

---
+++ 02Basics/myTupEnums.ts
@@ -0,0 +1,17 @@
+
+// const user : ( string | number )[] = [1,"vikas Dewangan"];
+
+let user : [string,boolean,number];
+
+user = ["vikas",true,21];
+
+let rgb :[number , number , number] = [255,255,255]; 
+
+type User = [number , string];
+
+const newUser : User = [234234,"vikas@gmail.com"];
+ 
+newUser[1] = "seth@gmail.com";
+// newUser.push(true); 
+
+export{};
