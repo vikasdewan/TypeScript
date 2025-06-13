d1d1ea40cd55d1ff7b49c250a4f259103b455418
VikasDewangan <dewanganvikas192@gmail.com>
2025-06-13 12:03:36 +0530

    generics done, understand the basics only not the whole concept of it, it's bit of confusing.

--- 03pureTS/dist/myGenerics.js
+++ 03pureTS/dist/myGenerics.js
@@ -16,3 +16,25 @@ function identityFour(val) {
     return val;
 }
 let miltonBootle = identityFour({ brand: "milton", quantity: 2 });
+function getSearchProducts(products) {
+    //do some database operations
+    const myIdx = 3;
+    return products[myIdx];
+}
+let getMoreSearchProducts = (products) => {
+    //do some database calculation
+    const myIdx = 4;
+    return products[myIdx];
+};
+function anotherFunction(valOne, valTwo) {
+    return { valOne, valTwo };
+}
+anotherFunction(3, { connection: "server", username: "debu", password: "23243" });
+class Sellable {
+    constructor() {
+        this.cart = [];
+    }
+    addToCart(product) {
+        this.cart.push(product);
+    }
+}
--- 03pureTS/src/myGenerics.ts
+++ 03pureTS/src/myGenerics.ts
@@ -29,3 +29,50 @@ interface Bottle {
 }
 
 let miltonBootle : Bottle  = identityFour<Bottle>({ brand :  "milton" , quantity : 2}) 
+
+function getSearchProducts<T,>(products : T[]) : T { // <T,> this define that this is a generic , and  here comma represent that it is not any JSX component or anything like that.
+
+    //do some database operations
+    const myIdx = 3;
+    return products[myIdx];
+}
+
+let getMoreSearchProducts = <T>(products:T[]): T => {
+    //do some database calculation
+    const myIdx = 4;
+    return products[myIdx];
+}
+
+
+
+interface Database {
+    connection : string,
+    username : string,
+    password : string
+}
+
+function anotherFunction<T,U extends Database>(valOne : T , valTwo : U):object {
+      return { valOne , valTwo };
+}
+
+anotherFunction(3,{connection:"server",username:"debu",password:"23243"});
+
+
+
+interface Quiz {
+    name:string,
+    type:string
+}
+
+interface Course {
+    name : string,
+    author : string,
+    subject : string
+}
+
+class Sellable<T>{
+    public cart: T[] = [];
+    addToCart(product : T){
+        this.cart.push(product);
+    } 
+}
